72f99afe9d699df46ac18f49b7339039f6a147f5
MaksimsTurs <maksims07turs@gmail.com>
2026-02-24 14:15:05 +0000

    feat: adds util functions for work with jwt

---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,48 @@
+import type { JwtPayload } from "jsonwebtoken";
+
+import jsonwebtoken from "jsonwebtoken";
+
+function generateToken(payload: string, secret: string): string {
+  return jsonwebtoken.sign(
+    payload, 
+    secret,
+    { expiresIn: "7 days" }
+  );
+};
+
+function verifyToken<P = unknown>(token: string = "", secret: string): P & JwtPayload | undefined {
+   if(!token) {
+    return undefined;
+  }
+
+  return jsonwebtoken.verify(token, secret) as P & JwtPayload; 
+};
+
+export function generateAccessToken(payload: any): string {
+  return generateToken(payload, process.env.ACCESS_TOKEN_SECRET as string);
+};
+
+export function generateRefreshToken(payload: any): string {
+  return generateToken(payload, process.env.REFRESH_TOKEN_SECRET as string);
+};
+
+export function verifyAccessToken<P = unknown>(token?: string): P & JwtPayload | undefined {
+  return verifyToken(token, process.env.ACCESS_TOKEN_SECRET!);
+};
+
+export function verifyRefreshToken<P = unknown>(token?: string): P & JwtPayload | undefined {
+  return verifyToken(token, process.env.REFRESH_TOKEN_SECRET!);
+};
+
+export function isBlackListed(token: string): boolean {
+  // TODO: Implement...
+  return false;
+};
+
+export function addTokenToBlackList(token: string): void {
+  // TODO: Implement...
+};
+
+export function removeTokenFromBlackList(token: string): void {
+  // TODO: Implement...
+};
