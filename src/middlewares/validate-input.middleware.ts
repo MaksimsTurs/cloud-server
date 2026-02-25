59e159d55157629fcf5974e4beb674ae3000b86b
MaksimsTurs <maksims07turs@gmail.com>
2026-02-25 01:10:33 +0000

    refactor: replace old middleware handlers with single validation-input function

---
+++ src/middlewares/validate-input.middleware.ts
@@ -0,0 +1,15 @@
+import type { VineValidator } from "@vinejs/vine";
+import type { NextFunction, Request, Response } from "express";
+
+type ValidationTarget = "body" | "params" | "query";
+
+export default function validate(what: ValidationTarget, schema: VineValidator<any, any>) {
+  return async function(req: Request, _res: Response, next: NextFunction) {
+    try {
+      await schema.validate(req[what]);
+      next();
+    } catch(error) {
+      next(error);
+    }
+  }
+};
