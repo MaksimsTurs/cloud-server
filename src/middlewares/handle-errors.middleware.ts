fe9605b42bc387758a9cee4a185e2fe4a48f1723
MaksimsTurs <maksims07turs@gmail.com>
2026-02-25 01:19:39 +0000

    refactor: process-errors renamed into handle-errors

--- src/middlewares/handle-errors.middleware.ts
+++ src/middlewares/handle-errors.middleware.ts
@@ -1,4 +1,6 @@
-import type { Response, Request } from "express";
+import type { Response, Request, NextFunction } from "express";
+
+import { errors } from "@vinejs/vine";
 
 import CaughtError from "../utils/Caught-Error.util";
 
@@ -6,10 +8,13 @@ import { logger } from "../index";
 
 import HTTP_ERRORS from "../const/HTTP-ERRORS.const";
 
-export default async function processError(error: unknown, req: Request, res: Response, _next): Promise<void> {
+export default async function processError(error: unknown, _req: Request, res: Response, _next: NextFunction): Promise<void> {
   if(error instanceof CaughtError) {
     res.status(error.options.client.code).send(error.options.client);
     logger.terminal.error(error.options.server.message);
+  } else if(error instanceof errors.E_VALIDATION_ERROR) {
+    res.status(401).send(HTTP_ERRORS.BAD_REQUEST(error.messages[0].message));
+    logger.terminal.error("Validation errors", error);
   } else if(error instanceof Error) {
     res.status(500).send(HTTP_ERRORS.INTERNAL_SERVER_ERROR());
     logger.terminal.error(error.message);
