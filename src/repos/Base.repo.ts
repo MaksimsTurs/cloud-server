bf6069717b317acc68d3f25ec7557bb64bf1bd74
MaksimsTurs <maksims07turs@gmail.com>
2026-02-25 20:36:07 +0000

    feat: adds isExist function to fast check if row with some value exist

--- src/repos/Base.repo.ts
+++ src/repos/Base.repo.ts
@@ -1,3 +1,5 @@
+import type { Row, RowList } from "postgres";
+
 import { sql } from "../index";
 
 class BaseRepository {
@@ -10,6 +12,11 @@ class BaseRepository {
   public async insert<D>(data: D): Promise<void> {
     const keys: string[] = Object.keys(data as any);
     await sql`INSERT INTO ${sql(this.name)} ${sql(data as any, keys)}`;
+  };
+
+  public async isExist(column: string, value: any): Promise<boolean> {
+    const res: RowList<Row[]> = await sql`SELECT ${sql(column)} FROM ${sql(this.name)} WHERE ${sql(column)} = ${value} LIMIT 1`;
+    return res.length != 0;
   };
 };
 
