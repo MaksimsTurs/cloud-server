eaf31954128959bd41402bcb666b9746591c45f2
MaksimsTurs <maksims07turs@gmail.com>
2026-03-05 15:14:14 +0100

    fix: imports

--- src/const/COOKIE.const.ts
+++ src/const/COOKIE.const.ts
@@ -1,7 +1,5 @@
 import type { CookieOptions } from "express";
 
-import { serverConfigs } from "..";
-
 export default {
   ACCESS_TOKEN_KEY: "LCLOUD_ACCESS_TOKEN",
   REFRESH_TOKEN_KEY: "LCLOUD_REFRESH_TOKEN",
@@ -10,13 +8,13 @@ export default {
     maxAge: 900_000,
     path: "/",
     sameSite: "lax",
-    secure: serverConfigs.MODE != "dev"
+    secure: process.env.MODE != "dev"
   } as CookieOptions,
   ACCESS_OPTIONS: {
     httpOnly: true,
     maxAge: 604_800_000,
     path: "/",
     sameSite: "lax",
-    secure: serverConfigs.MODE != "dev"
+    secure: process.env.MODE != "dev"
   } as CookieOptions
 } as const;
