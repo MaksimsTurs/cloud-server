59134ba6cafdbbda972fbaf9ef5a7fba2e78a405
MaksimsTurs <maksims07turs@gmail.com>
2026-03-05 15:02:39 +0100

    feat: add more queries, rename some queries and add some typesafety

--- src/repos/SQL.repo.ts
+++ src/repos/SQL.repo.ts
@@ -1,36 +1,51 @@
-import type { Row, RowList } from "postgres";
+import type { PendingQuery, Row, RowList } from "postgres";
+
+import joinSqlQueries from "../utils/join-sql-queries.util";
 
 import { sql } from "../index";
 
-class SQLRepository<T = unknown> {
+class SQLRepository<T extends object> {
   public table: string = "";
 
   constructor(table: string) {
     this.table = table;
   };
 
-  public async insert(data: T): Promise<void> {
+  public async insertOne(data: T): Promise<void> {
     const keys: string[] = Object.keys(data as any);
-    await sql`INSERT INTO ${sql(this.table)} ${sql(data as any, keys)}`;
+    await sql<T[]>`INSERT INTO ${sql(this.table)} ${sql(data as any, keys)}`;
   };
 
-  public async getBy(column: string, value: any): Promise<T | undefined> {
-    const res: T | undefined = (await sql`SELECT * FROM ${sql(this.table)} WHERE ${sql(column)} = ${value} LIMIT 1`).at(-1) as T | undefined;
-    return res;
+  public async getOne(data: Partial<T>): Promise<T | undefined> {
+    const conditions: PendingQuery<Row[]>[] = [];
+
+    for(let key in data) {
+      const value: any = data[key];
+      conditions.push(sql`${sql(key as string)} = ${value}`);
+    }
+
+    const filters: PendingQuery<Row[]>[] = joinSqlQueries(conditions, sql` AND `);
+    const res: RowList<T[]> = await sql<T[]>`SELECT * FROM ${sql(this.table)} WHERE ${filters} LIMIT 1`;
+    
+    return res.at(-1);
   };
 
   public async getById(id: string): Promise<T | undefined> {
-    const res: T | undefined = (await sql`SELECT * FROM ${sql(this.table)} WHERE id = ${id} LIMIT 1`).at(-1) as T | undefined;
-    return res;
+    const res: RowList<T[]> = await sql<T[]>`SELECT * FROM ${sql(this.table)} WHERE id = ${id} LIMIT 1`;
+    return res.at(-1);
+  };
+
+  public async removeOne(column: string, value: any): Promise<void> {
+    await sql<T[]>`DELETE FROM ${sql(this.table)} WHERE ${sql(column)} = ${value}`
   };
 
   public async updateById(id: string, data: Partial<T>): Promise<void> {
     const keys: string[] = Object.keys(data as any);
-    await sql`UPDATE ${sql(this.table)} SET ${sql(data as any, keys)} WHERE id = ${id}`;
+    await sql<T[]>`UPDATE ${sql(this.table)} SET ${sql(data as any, keys)} WHERE id = ${id}`;
   };
 
   public async isExist(column: string, value: any): Promise<boolean> {
-    const res: RowList<Row[]> = await sql`SELECT ${sql(column)} FROM ${sql(this.table)} WHERE ${sql(column)} = ${value} LIMIT 1`;
+    const res: RowList<Row[]> = await sql<T[]>`SELECT ${sql(column)} FROM ${sql(this.table)} WHERE ${sql(column)} = ${value} LIMIT 1`;
     return res.length != 0;
   };
 };
