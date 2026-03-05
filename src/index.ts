3063610b9b4ab8cc91525ee9623efde0bb98e57d
MaksimsTurs <maksims07turs@gmail.com>
2026-03-05 15:13:52 +0100

    feat: create of server config

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,26 @@ dotenv.config();
 import connectToPostgers from "./configs/postgres.config";
 import createLogger from "./configs/logger.config";
 import createUploader from "./configs/multer.config";
+import defineServerConfig from "./configs/define-server-config.config";
 
 import initUserRouter from "./routes/user/user.route";
-import initDirRouter from "./routes/dir/dir.route";
+import initObjectStorageRouter from "./routes/object-storage/object-storage.route";
 import defaultRoute from "./routes/404.route";
 
 import handleError from "./middlewares/handle-errors.middleware";
 
-import getCorsOrigin from "./utils/get-cors-origin.util";
-
+export const serverConfigs = defineServerConfig();
 export const logger = createLogger();
 export const sql = connectToPostgers();
 export const uploader = createUploader();
 const server = express();
 
 server
-  .use(cors({ origin: getCorsOrigin(), credentials: true }))
+  .use(cors({ origin: serverConfigs.ALLOWED_ORIGINS, credentials: true }))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .use(cookieParser())
-  .use("/user",     initUserRouter())
-  .post("/dir",     initDirRouter(uploader))
-  .all("/{*splat}", defaultRoute, handleError)
-  .listen(process.env.SERVER_PORT)
+  .use("/user",         initUserRouter())
+  .use("/storage",      initObjectStorageRouter(uploader))
+  .all("/{*splat}",     defaultRoute, handleError)
+  .listen(serverConfigs.PORT)
