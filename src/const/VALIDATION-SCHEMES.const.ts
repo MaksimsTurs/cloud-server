b8797684c8039ee0d263b031b6776ed82f5355db
MaksimsTurs <maksims07turs@gmail.com>
2026-03-05 14:48:13 +0100

    refactor: move identical schemas into variables

--- src/const/VALIDATION-SCHEMES.const.ts
+++ src/const/VALIDATION-SCHEMES.const.ts
@@ -4,13 +4,51 @@ import COOKIE from "./COOKIE.const";
 
 vine.convertEmptyStringsToNull = true;
 
+const StorageObjectScheme = vine.object({
+  id: vine
+    .string()
+    .trim()
+    .uuid({ version: [4] }),
+  user_id: vine
+    .string()
+    .trim()
+    .uuid({ version: [4] }),
+  parent_id: vine
+    .string()
+    .trim()
+    .uuid({ version: [4] })
+    .optional(),
+ name: vine
+    .string()
+    .trim()
+    .escape(),
+  type: vine
+    .number()
+});
+const PasswordScheme = vine
+  .string()
+  .trim()
+  .escape()
+  .minLength(12);
+const EmailScheme = vine
+  .string()
+  .trim()
+  .escape()
+  .maxLength(254)
+  .email();
+const UUIDScheme = vine
+  .string()
+  .trim()
+  .escape()
+  .uuid({ version: [4] });
+const JwtScheme = vine
+  .string()
+  .jwt();
+
 export default {
   // Directory route schemes
   DIR_UPLOAD: vine.create({
-    uploadPath: vine
-      .string()
-      .trim()
-      .escape()
+    parentId: UUIDScheme
   }),
   DIR_CREATE: vine.create({
     name: vine
@@ -18,107 +56,52 @@ export default {
       .maxLength(64)
       .trim()
       .escape(),
-    parentId: vine
+    parentId: UUIDScheme,
+    path: vine
       .string()
       .trim()
       .escape()
-      .uuid({ version: [4] })
-      .optional()
   }),
   DIR_READ: vine.create({
-    dir: vine
-      .string()
-      .trim()
-      .escape()
+    id: UUIDScheme
+      .clone()
       .optional()
   }),
-  DIR_REMOVE: vine.create({
-    fromPath: vine
-      .string()
-      .trim()
-      .escape(),
-    itemNames: vine
-      .array(vine
-            .string()
-            .trim()
-            .escape())
-    }),
-  DIR_MOVE: vine.create({
-    fromPath: vine
-      .string()
-      .trim()
-      .escape(),
-    intoPath: vine
-      .string()
-      .trim()
-      .escape(),
-    itemNames: vine
-      .array(vine
-            .string()
-            .trim()
-            .escape())
-    }),
+  DIR_READ_OBJECT: vine.create({
+    id: UUIDScheme
+  }),
+  DIR_REMOVE: vine.create(vine.record(StorageObjectScheme)),
   DIR_COPY: vine.create({
-    fromPath: vine
-      .string()
-      .trim()
-      .escape(),
-    intoPath: vine
-      .string()
-      .trim()
-      .escape(),
-    itemNames: vine
-      .array(vine
-            .string()
-            .trim()
-            .escape())
+    parentId: UUIDScheme,
+    items: vine.record(StorageObjectScheme)
+  }),
+  DIR_MOVE: vine.create({
+    parentId: UUIDScheme,
+    items: vine.record(StorageObjectScheme)
+  }),
+  LOG_IN: vine.create({
+    email: EmailScheme, 
+    password: PasswordScheme 
   }),
   // User route schemes
   LOG_UP: vine.create({
-    email: vine
-      .string()
-      .trim()
-      .escape()
-      .maxLength(254)
-      .email(),
-    password: vine
-      .string()
-      .trim()
-      .escape()
-      .minLength(12)
+    email: EmailScheme,
+    password: PasswordScheme
+      .clone()
       .confirmed({ as: "confirmPassword" }),
-    confirmPassword: vine
-      .string()
-      .trim()
-      .escape()
-      .minLength(12)
-      .sameAs("password")
-  }),
-  LOG_IN: vine.create({
-    email: vine
-      .string()
-      .trim()
-      .escape()
-      .maxLength(254)
-      .email(),
-    password: vine
-      .string()
-      .trim()
-      .escape()
-      .minLength(12)
+    confirmPassword: PasswordScheme
+      .clone()
+      .sameAs("password") 
   }),
   // Middleware route schemes
   JWT_PAYLOAD: vine.create({
-    id: vine
-      .string()
-      .uuid({ version: [4] })
+    id: UUIDScheme
+  }),
+  REFRESH_TOKEN: vine.create({
+    [COOKIE.ACCESS_TOKEN_KEY]: JwtScheme
   }),
   AUTH: vine.create({
-    [COOKIE.REFRESH_TOKEN_KEY]: vine
-      .string()
-      .jwt(),
-    [COOKIE.ACCESS_TOKEN_KEY]: vine
-      .string()
-      .jwt()
+    [COOKIE.REFRESH_TOKEN_KEY]: JwtScheme,
+    [COOKIE.ACCESS_TOKEN_KEY]: JwtScheme
   }),
 };
