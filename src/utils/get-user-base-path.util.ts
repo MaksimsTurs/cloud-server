5902a57756fc48556329ad4345a1e6c45985670c
MaksimsTurs <maksims07turs@gmail.com>
2026-03-05 14:57:53 +0100

    refactor: replace getUserBasePath with server config variables

--- src/utils/get-user-base-path.util.ts
+++
@@ -1,7 +0,0 @@
-export default function getUserBasePath(): string {
-  const mode: string = process.env.MODE as string;
-  const devBasePath: string = process.env.DEV_BASE_PATH as string;
-  const prodBasePath: string = process.env.PROD_BASE_PATH as string;
-
-  return mode === "dev" ? devBasePath : prodBasePath;
-};
