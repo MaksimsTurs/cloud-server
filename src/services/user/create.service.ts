d638731fb5b2e73ed3c0eb488f073d7e23f7bca8
MaksimsTurs <maksims07turs@gmail.com>
2026-03-14 19:29:35 +0100

    refactor: small changes

--- src/services/user/create.service.ts
+++ src/services/user/create.service.ts
@@ -1,15 +1,14 @@
-import type { User, UserTokens } from "../../index.type";
+import type { User } from "../../index.type";
 import type { UserServiceCreateReturn } from "./user-service.type";
 import type { UserLogUpReqBody } from "../../routes/user/user-route.type";
 
 import CaughtError from "../../utils/Caught-Error.util";
 import generateId from "../../utils/generate-id.util";
+import { generateRefreshToken, generateAccessToken } from "../../utils/jwt/jwt.util";
 
 import userRepo from "../../repos/User.repo";
 
-import userService from "./user.service";
-
-import argon2 from "argon2";
+import argon from "argon2";
 
 import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
 
@@ -25,9 +24,10 @@ export default async function create(data: UserLogUpReqBody): Promise<UserServic
     });
   }
 
-  const hash: string = await argon2.hash(data.password);
   const id: string = generateId();
-  const tokens: UserTokens = userService.generateTokens(id);
+  const hash: string = await argon.hash(data.password);
+  const access: string = generateAccessToken({ id });
+  const refresh: string = generateRefreshToken({ id });
   const workDir: string = `${serverConfigs.BASE_USERS_PATH}/${id}`;
   const user: User = {
     id,
@@ -39,6 +39,6 @@ export default async function create(data: UserLogUpReqBody): Promise<UserServic
   return {
     user,
     workDir,
-    tokens
+    tokens: { access, refresh }
   };
 };
