2fdca46374851a93d5fa625475994632dad7044d
MaksimsTurs <maksims07turs@gmail.com>
2026-03-14 19:26:08 +0100

    refactor: remove getById service call

--- src/routes/user/log-out.route.ts
+++ src/routes/user/log-out.route.ts
@@ -1,29 +1,11 @@
 import type { Request, Response } from "express";
-import type { User } from "../../index.type";
-import type { UserLogOutResLocals } from "./user-route.type";
 
-import userService from "../../services/user/user.service";
-
-import CaughtError from "../../utils/Caught-Error.util";
-
-import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
 import COOKIE from "../../const/COOKIE.const";
 
 export default async function logOut(
-  req: Request,
-  res: Response<unknown, UserLogOutResLocals>
+  _req: Request,
+  res: Response
 ): Promise<void> {
-  const user: User | undefined = await userService.getById(res.locals.userId);
-
-  if(!user) {
-    throw new CaughtError({
-      server: {
-        message: `Unknown user ${req.socket.remoteAddress} has tried to log out`
-      },
-      client: HTTP_ERRORS.NOT_FOUND("User not found!")
-    });
-  }
-
   res.clearCookie(COOKIE.ACCESS_TOKEN_KEY, COOKIE.ACCESS_OPTIONS);
   res.clearCookie(COOKIE.REFRESH_TOKEN_KEY, COOKIE.REFRESH_OPTIONS);
   res.sendStatus(200);
