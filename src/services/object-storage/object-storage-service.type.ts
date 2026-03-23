f3a4607429dcbf27beb43392c41a72e1980790a0
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:10:31 +0100

    feat: add types for some services

--- src/services/object-storage/object-storage-service.type.ts
+++ src/services/object-storage/object-storage-service.type.ts
@@ -4,3 +4,9 @@ export type ObjectStorageServiceGetAllReturn = {
   items: StorageObject[]
   parent: StorageObject
 };
+
+export type ObjectStorageServiceCreateParam = {
+  id?: string
+} & Omit<StorageObject, "id">;
+
+export type ObjectStorageServiceCopyReturn = StorageObject[];
