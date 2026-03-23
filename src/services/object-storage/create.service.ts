685180f0da45aa6c8a44ba80ffe12486877afba2
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:07:46 +0100

    fix: parameters type error

--- src/services/object-storage/create.service.ts
+++ src/services/object-storage/create.service.ts
@@ -1,12 +1,12 @@
 import type { StorageObject } from "../../index.type";
+import type { ObjectStorageServiceCreateParam } from "./object-storage-service.type";
 
 import generateId from "../../utils/generate-id.util";
 
-export default async function create(data: StorageObject): Promise<StorageObject> {
-  const id: string = generateId();
+export default async function create(data: ObjectStorageServiceCreateParam): Promise<StorageObject> {
   const newObject: StorageObject = {
     ...data,
-    id: data.id || id,
+    id: data.id || generateId(),
   };
 
   return newObject;
