e72edd258dfab856cf885b70edcaefbedf2b035e
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 13:55:45 +0100

    refactor(route/404): migrate to new CaughtError version

--- src/routes/404.route.ts
+++ src/routes/404.route.ts
@@ -1,14 +1,13 @@
 import type { Request } from "express";
 
-import HTTP_ERRORS from "../const/HTTP-ERRORS.const";
-
 import CaughtError from "../utils/Caught-Error.util";
 
+import HTTP_ERROR_CODES from "../const/HTTP_ERROR_CODES.const";
+
 export default function defaultRoute(req: Request): void {
-  throw new CaughtError({
-    server: {
-      message: `Remote user ${req.socket.remoteAddress} has requested unknown route path ${req.path}`
-    },
-    client: HTTP_ERRORS.NOT_FOUND("Path not found!")
-  });
+  throw new CaughtError(
+    HTTP_ERROR_CODES.NOT_FOUND,
+    `Remote user ${req.socket.remoteAddress} has requested unknown route path ${req.path}`,
+    "Path not found!"
+  );
 };
