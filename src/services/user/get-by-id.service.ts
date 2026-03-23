98c9cf40b1e48cde7741e27dfc396fbbc3dd61e7
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:11:40 +0100

    refactor(service/user/create): migrate to new CaughtError version

--- src/services/user/get-by-id.service.ts
+++ src/services/user/get-by-id.service.ts
@@ -3,5 +3,5 @@ import type { User } from "../../index.type";
 import userRepo from "../../repos/User.repo";
 
 export default async function getById(id: string): Promise<User | undefined> {
-  return userRepo.getById(id);
+  return await userRepo.getById(id);
 };
