e638d941a1d6db6812e817d79e94af173276d0b3
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 13:54:38 +0100

    refactor(middleware/is-verified): migrate to new CaughtError version

--- src/middlewares/is-verified.middleware.ts
+++ src/middlewares/is-verified.middleware.ts
@@ -2,18 +2,17 @@ import type { Request, Response, NextFunction } from "express";
 
 import CaughtError from "../utils/Caught-Error.util";
 
-import HTTP_ERRORS from "../const/HTTP-ERRORS.const";
+import HTTP_ERROR_CODES from "../const/HTTP_ERROR_CODES.const";
 
 export default async function isVerified(req: Request, res: Response, next: NextFunction): Promise<void> {
   const { user } = res.locals;
 
   if(!user.is_verified) {
-    throw new CaughtError({
-      server: {
-        message: `Unverified user ${user.id} has tried to access ${req.path} path`
-      },
-      client: HTTP_ERRORS.FORBIDDEN("You are not verified!")
-    });
+    throw new CaughtError(
+      HTTP_ERROR_CODES.FORBIDDEN,
+      `Unverified user ${user.id} has tried to access ${req.path} path`,
+      "You are not verified!"
+    );
   }
 
   next();
