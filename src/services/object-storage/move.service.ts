9369435887fdc768a0a4444fdd7d95dc8cffe89d
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:09:03 +0100

    refactor(service/object-storage/move): removed redundat db request

--- src/services/object-storage/move.service.ts
+++ src/services/object-storage/move.service.ts
@@ -3,21 +3,20 @@ import type { ObjectStorageMoveObjectsReqBody } from "../../routes/object-storag
 
 import CaughtError from "../../utils/Caught-Error.util";
 
-import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
-
 import objectStorageRepo from "../../repos/Object-Storage.repo";
+
+import HTTP_ERROR_CODES from "../../const/HTTP_ERROR_CODES.const.ts";
 
 export default async function move(user: User, body: ObjectStorageMoveObjectsReqBody): Promise<void> {
   const items: Record<string, StorageObject> = body.items;
   const parent: StorageObject | undefined = await objectStorageRepo.getById(body.parentId);
 
   if(!parent) {
-    throw new CaughtError({
-      server: {
-        message: `${user.id} has tried to move items into not existing directory!`
-      },
-      client: HTTP_ERRORS.BAD_REQUEST("You can not move items into not existing directory!")
-    });
+    throw new CaughtError(
+      HTTP_ERROR_CODES.BAD_REQUEST,
+      `${user.id} has tried to move items into not existing directory!`,
+      "You can not move items into not existing directory!"
+    );
   }
 
   for(let name in items) {
@@ -25,12 +24,11 @@ export default async function move(user: User, body: ObjectStorageMoveObjectsReq
     const isExist: boolean = !!(await objectStorageRepo.getOne({ parent_id: parent.id, name: item.name }));
 
     if(isExist) {
-      throw new CaughtError({
-        server: {
-          message: `${user.id} has tried to move item (${item.name}) that already exist in ${parent.id} directory`
-        },
-        client: HTTP_ERRORS.CONFLICT("Item with the same name already exist!")
-      });
+      throw new CaughtError(
+        HTTP_ERROR_CODES.CONFLICT,
+        `${user.id} has tried to move item (${item.name}) that already exist in ${parent.id} directory`,
+        "Item with the same name already exist!"
+      );
     }
 
     await objectStorageRepo.updateById(item.id, { parent_id: parent.id });
