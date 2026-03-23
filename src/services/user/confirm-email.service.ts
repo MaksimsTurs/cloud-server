55eb8033373b189b341276973503533c423b36ba
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:11:05 +0100

    refactor(route/user/confirm-email): migrate to new CaughtError version

--- src/services/user/confirm-email.service.ts
+++ src/services/user/confirm-email.service.ts
@@ -6,18 +6,17 @@ import CaughtError from "../../utils/Caught-Error.util";
 
 import userRepo from "../../repos/User.repo";
 
-import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
+import HTTP_ERROR_CODES from "../../const/HTTP_ERROR_CODES.const";
 
 export default async function confirmEmail(token: string): Promise<void> {
   const payload: JwtTokenPaylaod<UserJwtPayload> | undefined = verifyEmailConfirmToken<UserJwtPayload>(token);
   
   if(!payload) {
-    throw new CaughtError({
-      server: {
-        message: `Unknown user has tried to confirm email`
-      },
-      client: HTTP_ERRORS.BAD_REQUEST("You email confirm token is not valid!")
-    });
+    throw new CaughtError(
+      HTTP_ERROR_CODES.BAD_REQUEST,
+      "Unknown user has tried to confirm email",
+      "You email confirm token is not valid!"
+    );
   }
 
   await userRepo.updateById(payload.id, { is_verified: true });
