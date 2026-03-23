bc798125c49853adb3bbc3a224d99bb1bc8956b2
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 14:08:44 +0100

    refactor(service/object-storage/get-all): removed redundat db request

--- src/services/object-storage/get-all.service.ts
+++ src/services/object-storage/get-all.service.ts
@@ -5,7 +5,7 @@ import storageRepo from "../../repos/Object-Storage.repo";
 
 import CaughtError from "../../utils/Caught-Error.util";
 
-import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
+import HTTP_ERROR_CODES from "../../const/HTTP_ERROR_CODES.const";
 
 export default async function getAll(user: User, id?: string): Promise<ObjectStorageServiceGetAllReturn> {
   let parentId: string = id || user.id;
@@ -16,13 +16,15 @@ export default async function getAll(user: User, id?: string): Promise<ObjectSto
   parent = items.at(0);
 
   if(!parent || parent.id !== parentId) {
-    throw new CaughtError({
-      server: {
-        message: `Can not find parent directory with id ${parentId}`
-      },
-      client: HTTP_ERRORS.NOT_FOUND("Can not find directory!")
-    });
+    throw new CaughtError(
+      HTTP_ERROR_CODES.NOT_FOUND,
+      `Can not find parent directory with id ${parentId}`,
+      "Can not find directory!"
+    );
   }
 
-  return { items: items.slice(1, items.length), parent };
+  return { 
+    items: items.slice(1, items.length), 
+    parent 
+  };
 };
