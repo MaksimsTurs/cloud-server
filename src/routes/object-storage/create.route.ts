fc0ede5bbb249d1fb7b13f623be146740442697e
MaksimsTurs <maksims07turs@gmail.com>
2026-03-23 13:57:46 +0100

    refactor(route/object-storage/create): removed redundat db request

--- src/routes/object-storage/create.route.ts
+++ src/routes/object-storage/create.route.ts
@@ -1,39 +1,23 @@
 import type { Request, Response } from "express";
-import type { StorageObject, User } from "../../index.type";
+import type { StorageObject } from "../../index.type";
 import type { ObjectStorageCreateItemReqBody, ObjectStorageCreateItemResLocals } from "./object-storage-route.type";
 
-import userService from "../../services/user/user.service";
 import objectStorageService from "../../services/object-storage/object-storage.service";
 
-import CaughtError from "../../utils/Caught-Error.util";
-
-import HTTP_ERRORS from "../../const/HTTP-ERRORS.const";
 import DIRI_TEM_TYPES from "../../const/DIR-ITEM-TYPES.const";
 
 export default async function create(
   req: Request<unknown, unknown, ObjectStorageCreateItemReqBody>,
   res: Response<StorageObject, ObjectStorageCreateItemResLocals>
 ): Promise<void> {
-  const user: User | undefined = await userService.getById(res.locals.userId);
-
-  if(!user) {
-    throw new CaughtError({
-      server: {
-        message: `Unknown user ${req.socket.remoteAddress} has tried to create new item`
-      },
-      client: HTTP_ERRORS.FORBIDDEN("You have no permission to create new item!")
-    });
-  }
-
-  //TODO Fix this
-  //@ts-ignore
+  const { user } = res.locals;
   const storageObject: StorageObject = await objectStorageService.create({
     user_id: user.id,
     type: DIRI_TEM_TYPES.DIR,
     name: req.body.name,
     parent_id: req.body.parentId,
   });
- 
+
   await objectStorageService.save(storageObject);
 
   res.status(200).send(storageObject);
