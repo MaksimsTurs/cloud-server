0c5a1f03cfda6410dadc3a0e31dfad3a685c62cf
MaksimsTurs <maksims07turs@gmail.com>
2026-06-01 19:40:03 +0200

    refactor: remove redundant utils

--- src/utils/is-path-has-base.util.ts
+++
@@ -1,3 +0,0 @@
-export default function isPathHasBase(base: string, path: string): boolean {
-  return path.startsWith(base);
-};
--- src/utils/is-path-secure.util.ts
+++
@@ -1,3 +0,0 @@
-export default function isPathSecure(path: string): boolean {
-  return /^\/?([A-Za-z0-9])\/?.+/.test(path);
-};
