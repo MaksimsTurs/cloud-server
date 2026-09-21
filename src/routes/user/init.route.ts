99ccdce8491d2ba13048bc1d4701a8f2b1abe3f9
MaksimsTurs <maksims07turs@gmail.com>
2026-09-21 12:51:40 +0200

    refactor: add types to res param

--- src/routes/user/init.route.ts
+++ src/routes/user/init.route.ts
@@ -1,24 +1,19 @@
 import type { Request, Response } from "express";
+import type { UserInitResBody, UserInitResLocals } from "./user-route.type";
 
 import COOKIE from "../../const/COOKIE.const";
 
-import userService from "../../services/user/user.service";
-
 export default async function init(
   req: Request, 
-  res: Response
+  res: Response<UserInitResBody, UserInitResLocals>
 ): Promise<void> {
-  const { accessToken, refreshToken, is_verified } = await userService.init(req.cookies[COOKIE.REFRESH_TOKEN_KEY]);
-
-  res.cookie(COOKIE.ACCESS_TOKEN_KEY, accessToken, COOKIE.ACCESS_OPTIONS);
-  res.cookie(COOKIE.REFRESH_TOKEN_KEY, refreshToken, COOKIE.REFRESH_OPTIONS);
   res.status(200).send({
     tokens: {
-      access: accessToken,
-      refresh: refreshToken
+      access: req.cookies[COOKIE.ACCESS_TOKEN_KEY],
+      refresh: req.cookies[COOKIE.REFRESH_TOKEN_KEY]
     },
     user: {
-      is_verified
+      is_verified: res.locals.user.is_verified
     }
   });
 };
